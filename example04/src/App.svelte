4855f9584a356e818e0bea6fe61835a92ca8adb1
Ivano Coltellacci <ivano.coltellacci@gmail.com>
2024-04-15 16:02:40 +0200

    Example04: Event Dispatcher

---
+++ example04/src/App.svelte
@@ -0,0 +1,52 @@
+<script>
+  import Popup from "./lib/popup.svelte";
+
+  let showPoup = false;
+
+  let show_popup = () => {
+    showPoup = true;
+  };
+
+  let close_popup = () => {
+    showPoup = false;
+  };
+</script>
+
+<main>
+  <h1 class="pb-5">Event Dispatcher</h1>
+
+  <button class="btb btn-outline btn-primary" on:click={show_popup}>Show Popup</button>
+
+  {#if showPoup}
+    <Popup on:close_popup_event={close_popup} />
+  {/if}
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    font-weight: 100;
+  }
+
+  :global(h1) {
+    color: firebrick;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  :global(h2) {
+    color: firebrick;
+    text-transform: none;
+    font-size: 2.5em;
+    font-weight: 50;
+  }
+
+  @media (min-width: 600px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ example04/src/lib/popup.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+</script>
+
+<main>
+  <div class="absolute top-0 left-0 right-0 flex justify-center">
+    <div class="bg-gradient-to-r from-purple-300 to-purple-500 p-5 rounded-lg">
+      <h2 class="pb-5">This is a Popup</h2>
+      <button
+        class="btn btn-outline btn-primary"
+        on:click={() => {
+          dispatch("close_popup_event");
+        }}>Close</button
+      >
+    </div>
+  </div>
+</main>
