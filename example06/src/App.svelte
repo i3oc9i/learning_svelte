1cfc720c084c435407082e1382049bfc4c46a785
Ivano Coltellacci <ivano.coltellacci@gmail.com>
2024-04-15 17:51:26 +0200

    Example06: Slots

---
+++ example06/src/App.svelte
@@ -0,0 +1,71 @@
+<script>
+  import Card from "./lib/Card.svelte";
+</script>
+
+<main>
+  <h1>Slots</h1>
+
+  <Card>
+    <div slot="header">
+      <h2>Picsum</h2>
+    </div>
+    <div slot="body">
+      <img src="https://picsum.photos/100/100" alt="Vite" />
+    </div>
+    <div slot="footer">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua.
+      </p>
+    </div>
+  </Card>
+  <Card>
+    <div slot="header">
+      <h2>Picsum</h2>
+    </div>
+    <div slot="body">
+      <img src="https://picsum.photos/100/150" alt="Vite" />
+    </div>
+  </Card>
+  <Card>
+    <div slot="header">
+      <h2>Vite</h2>
+    </div>
+    <div slot="body">
+      <img src="./public/vite.svg" alt="Vite" />
+    </div>
+    <div slot="footer">
+      <button>More Info</button>
+    </div>
+  </Card>
+  <Card />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    font-weight: 100;
+  }
+
+  :global(h1) {
+    color: firebrick;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  :global(h2) {
+    color: firebrick;
+    text-transform: none;
+    font-size: 2.5em;
+    font-weight: 50;
+  }
+
+  @media (min-width: 600px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ example06/src/lib/Card.svelte
@@ -0,0 +1,35 @@
+<script>
+</script>
+
+<main>
+  <div class="card">
+    <div class="card-header">
+      <slot name="header">header</slot>
+    </div>
+    <div class="card-body">
+      <slot name="body">
+        <img src="./src/assets/svelte.svg" alt="Svelte" />
+      </slot>
+    </div>
+    {#if $$slots.footer}
+      <hr />
+      <div class="card-footer">
+        <slot name="footer">footer</slot>
+      </div>
+    {/if}
+  </div>
+</main>
+
+<style>
+  .card {
+    box-shadow: 0 4px 8px 0 rgba(212, 96, 96, 0.783);
+    transition: 0.3s;
+    padding: 16px;
+    margin-bottom: 20px;
+    width: 200px;
+  }
+
+  .card:hover {
+    box-shadow: 0 8px 16px 0 rgba(212, 96, 96, 0.783);
+  }
+</style>
