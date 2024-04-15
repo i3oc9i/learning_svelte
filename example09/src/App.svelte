1ab4b1464caaf91abff44676bb9a53b2ad18df7a
Ivano Coltellacci <ivano.coltellacci@gmail.com>
2024-04-15 21:02:42 +0200

    Example09: Context Module

---
+++ example09/src/App.svelte
@@ -0,0 +1,50 @@
+<script>
+  import Counter, { total_count } from "./lib/Counter.svelte";
+</script>
+
+<main>
+  <h1>Context Module</h1>
+  <Counter />
+  <Counter />
+  <Counter />
+
+  <hr />
+  <div>
+    <button
+      on:click={() => {
+        alert(`Total count is now: ${total_count}`);
+      }}
+    >
+      total count
+    </button>
+  </div>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    font-weight: 100;
+  }
+
+  :global(h1) {
+    color: firebrick;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  :global(h2) {
+    color: firebrick;
+    text-transform: none;
+    font-size: 2.5em;
+    font-weight: 50;
+  }
+
+  @media (min-width: 600px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ example09/src/lib/Counter.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+  export let total_count = 0;
+</script>
+
+<script>
+  let count = 0;
+  const increment = () => {
+    count += 1;
+    total_count += 1;
+  };
+</script>
+
+<button on:click={increment}>
+  count is {count}
+</button>
